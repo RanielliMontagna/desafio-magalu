250f2d18f30a85130b9a64d2e2fafd84f30f20d7
Ranielli Montagna <raniellimontagna@hotmail.com>
2023-08-31 20:20:07 -0300

    feat: :sparkles: implementa repositório para usuário do sistema e cria caso de uso de autenticação

---
+++ service/src/repositories/in-memory/in-memory-user-repository.ts
@@ -0,0 +1,42 @@
+import { randomUUID } from 'node:crypto'
+
+import type { Prisma, User } from '@prisma/client'
+import type { UserRepository } from '../user-repository'
+
+export class InMemoryUserRepository implements UserRepository {
+  public users: User[] = []
+
+  async findById(id: string) {
+    const user = this.users.find((user) => user.id === id)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  async findByEmail(email: string) {
+    const user = this.users.find((user) => user.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  async create(user: Prisma.UserUncheckedCreateInput) {
+    const newUser: User = {
+      id: user.id || randomUUID(),
+      name: user.name || null,
+      email: user.email,
+      password_hash: user.password_hash,
+      created_at: new Date(),
+    }
+
+    this.users.push(newUser)
+
+    return newUser
+  }
+}
---
+++ service/src/use-cases/users/authenticate-user.spec.ts
@@ -0,0 +1,67 @@
+import { hash } from 'bcryptjs'
+import { expect, describe, it, beforeEach } from 'vitest'
+
+import { InMemoryUserRepository } from '@/repositories/in-memory/in-memory-user-repository'
+import { AuthenticateUserUseCase } from './authenticate-user'
+
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { createHashPassword } from '@/utils/http/create-hash-password'
+
+let usersRepository: InMemoryUserRepository
+let sut: AuthenticateUserUseCase
+
+describe('Authenticate Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUserRepository()
+    sut = new AuthenticateUserUseCase(usersRepository)
+  })
+
+  it('should be able to authenticate a user', async () => {
+    await usersRepository.create({
+      name: 'User Name',
+      email: 'user@name.com',
+      password_hash: await createHashPassword('a1s2d3'),
+    })
+
+    const { user } = await sut.execute({
+      email: 'user@name.com',
+      password: 'a1s2d3',
+    })
+
+    console.log(user)
+
+    expect(user).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        name: 'User Name',
+        email: 'user@name.com',
+        password_hash: expect.any(String),
+        created_at: expect.any(Date),
+      }),
+    )
+  })
+
+  it('should not be able to authenticate a user with invalid email', async () => {
+    await expect(
+      sut.execute({
+        email: 'teste@gmail.com',
+        password: 'password',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+
+  it('should not be able to authenticate a user with invalid password', async () => {
+    await usersRepository.create({
+      name: 'User Name',
+      email: 'user@name.com',
+      password_hash: await hash('teste123', 8),
+    })
+
+    await expect(
+      sut.execute({
+        email: 'user@name.com',
+        password: 'teste321',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+})
