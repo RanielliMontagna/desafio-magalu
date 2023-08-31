7fa86b3542ab72072a1a60f8646a6fc7bdec68dc
Ranielli Montagna <raniellimontagna@hotmail.com>
2023-08-31 20:00:52 -0300

    feat: :sparkles: implementa estrutura para banco de dados utilizando `prisma` e `docker` e prepara ambiente para testes

---
+++ service/prisma/migrations/20230831224833_create_database/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "password_hash" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ceps" (
+    "cep" TEXT NOT NULL,
+    "rua" TEXT NOT NULL,
+    "cidade" TEXT NOT NULL,
+    "estado" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ceps_cep_key" ON "ceps"("cep");
---
+++ service/prisma/seed.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from '@prisma/client'
+
+import { createHashPassword } from '../src/utils/http/create-hash-password'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const alice = await prisma.user.upsert({
+    where: { email: 'alice@prisma.io' },
+    update: {},
+    create: {
+      email: 'alice@prisma.io',
+      name: 'Alice',
+      password_hash: await createHashPassword('a1s2d3'),
+    },
+  })
+  const bob = await prisma.user.upsert({
+    where: { email: 'bob@prisma.io' },
+    update: {},
+    create: {
+      email: 'bob@prisma.io',
+      name: 'Bob',
+      password_hash: await createHashPassword('a1s2d3'),
+    },
+  })
+
+  console.log({ alice, bob })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
