abcb5b43ce1264b09866e11d8b2e0346ffddfe7f
Ranielli Montagna <raniellimontagna@hotmail.com>
2023-09-01 23:04:12 -0300

    refactor(service): :recycle: faz ajusta de cors para funcionar em produção

--- service/src/app.ts
+++ service/src/app.ts
@@ -26,7 +26,11 @@ Sentry.init({
 })
 
 app.register(cors, {
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'https://desafio-tecnico-luizalabs-web.onrender.com',
+  ],
 })
 
 app.register(fastifyJwt, {
